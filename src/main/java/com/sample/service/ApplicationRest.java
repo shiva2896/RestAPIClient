cb03fba43095bfdb45561a0beae6be3b344707e3
shiva2896 <67514985+shiva2896@users.noreply.github.com>
2020-06-29 11:19:22 +0530

    Add files via upload

--- src/main/java/com/sample/service/ApplicationRest.java
+++ src/main/java/com/sample/service/ApplicationRest.java
@@ -1,1 +1,23 @@
+package com.sample.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@ComponentScan(value={ "com.sample"} )
+public class ApplicationRest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SpringApplication.run(ApplicationRest.class, args);
+	}
+
+	@Bean
+	   public RestTemplate getRestTemplate() {
+	      return new RestTemplate();
+	   }
+	
+}
