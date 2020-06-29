c3a14de36d18e905569f913793eddf3d4c42f2f0
shiva2896 <67514985+shiva2896@users.noreply.github.com>
2020-06-29 11:23:54 +0530

    Add files via upload

--- src/main/java/com/sample/dao/Request.java
+++ src/main/java/com/sample/dao/Request.java
@@ -1,1 +1,111 @@
 
+package com.sample.dao;
+
+import javax.annotation.Generated;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+@Generated("org.jsonschema2pojo")
+public class Request {
+
+    private String name;
+    private String city;
+    private String state;
+    private String streetAddress;
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return
+     *     The city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * 
+     * @param city
+     *     The city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * 
+     * @return
+     *     The state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * 
+     * @param state
+     *     The state
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * 
+     * @return
+     *     The streetAddress
+     */
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    /**
+     * 
+     * @param streetAddress
+     *     The streetAddress
+     */
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(name).append(city).append(state).append(streetAddress).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Request) == false) {
+            return false;
+        }
+        Request rhs = ((Request) other);
+        return new EqualsBuilder().append(name, rhs.name).append(city, rhs.city).append(state, rhs.state).append(streetAddress, rhs.streetAddress).isEquals();
+    }
+
+}
---
+++ src/main/java/com/sample/dao/Response.java
@@ -0,0 +1,73 @@
+
+package com.sample.dao;
+
+import javax.annotation.Generated;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+@Generated("org.jsonschema2pojo")
+public class Response {
+
+    private String status;
+    private String name;
+
+    /**
+     * 
+     * @return
+     *     The status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * 
+     * @param status
+     *     The status
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(status).append(name).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Response) == false) {
+            return false;
+        }
+        Response rhs = ((Response) other);
+        return new EqualsBuilder().append(status, rhs.status).append(name, rhs.name).isEquals();
+    }
+
+}
